3ab706ecf71663b8cfd28318ead2adaa8ac01021
ESJ0 <usuarioj0s3p1@gmail.com>
2025-03-15 13:17:01 -0600

    Clase "Merchandising" creada que se ncarga del manejo de toda la logica de los productos.

---
+++ Merchandising.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Merchandising {
+    String SKU, Nombre, Descripcion;
+    Map<String, Integer> TallasDisponibles;
+
+    public Merchandising(String SKU, String Nombre, String Descripcion, String Tallas) {
+        this.SKU = SKU;
+        this.Nombre = Nombre;
+        this.Descripcion = Descripcion;
+        this.TallasDisponibles = parseTallas(Tallas);
+    }
+
+    private Map<String, Integer> parseTallas(String data) {
+        Map<String, Integer> tallasMap = new HashMap<>();
+        String[] parts = data.split("\\|");
+        for (String part : parts) {
+            String[] kv = part.split(":");
+            tallasMap.put(kv[0], Integer.parseInt(kv[1]));
+        }
+        return tallasMap;
+    }
+
+    @Override
+    public String toString() {
+        return "SKU: " + SKU + ", Nombre: " + Nombre + ", Descripcion: " + Descripcion + ", TallasDisponibles: " + TallasDisponibles;
+    }
+}
